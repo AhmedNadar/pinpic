2d782756f30e931a2349d777fcaad933e5942861
Ahmed Nadar <ahmedfay@hotmail.com>
2013-11-21 11:25:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinpic::Application.config.secret_key_base = '399ade3327d81df17be863f97e1d8087bcdaaf35683a2e0a9da10188164d0cbbef3187cf1cb45f362ae7e059c125fb43cab3de9318fcc1268a0b4f1c7f9bbd7c'
